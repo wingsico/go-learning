7de109da6aad18cfe9eafedb80b5e967b8089302
wingsico <wingsico@gmail.com>
2020-01-04 16:03:40 +0800

    refactor: 💡 提取断言方法

--- hello/hello.go
+++ hello/hello.go
@@ -10,5 +10,8 @@ const enHelloPrefix = "Hello, "
 
 // Hello : get Hello String
 func Hello(s string) string {
+	if s == "" {
+		s = "world"
+	}
 	return enHelloPrefix + s
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("wingsico")
-	want := "Hello, wingsico"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("wingsico")
+		want := "Hello, wingsico"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+
+	})
 }
