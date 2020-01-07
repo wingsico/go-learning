c6956c327ed41cc2ce2b6a08ac1d226f82a3aa27
wingsico <wingsico@gmail.com>
2020-01-07 15:33:43 +0800

    refactor: 💡 使用range

--- array/sum.go
+++ array/sum.go
@@ -3,8 +3,9 @@ package array
 // Sum get a sum of a array
 func Sum(a []int) (sum int) {
 	sum = 0
-	for i := 0; i < len(a); i++ {
-		sum += a[i]
+
+	for _, n := range a {
+		sum += n
 	}
 	return sum
 }
--- array/sum_test.go
+++ array/sum_test.go
@@ -6,7 +6,7 @@ func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
-	expected := 14
+	expected := 15
 
 	if got != expected {
 		t.Errorf("got %v, expected %v, %v", got, expected, numbers)
